9e8dcdb977064094b2a5eb575082bb6066eccb88
Adam Reed <adam.rdconcepts@gmail.com>
2017-08-04 23:47:06 -0700

    add timestamp, email validation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from momentjs import momentjs
 
 
 app = Flask(__name__)
@@ -9,7 +11,10 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = "abc"
 
-
+#TASK USER CLASS, CURRENTLY FILTERS BY
+    # USER ACCOUNT
+    # QUADRANT (1-6)
+    # COMPLETED (TRUE OR FALSE)
 class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
@@ -17,18 +22,16 @@ class Task(db.Model):
     completed = db.Column(db.Boolean)
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     quad_id = db.Column(db.Integer)
-
-    #TODO how to figure out to assign the quadrant clicked with the variable
+    created = db.Column(db.DateTime)
 
     def __init__(self, name, owner, quad_id):
         self.name = name
         self.completed = False
+        self.created  = datetime.utcnow()
         self.owner = owner
         self.quad_id = quad_id
 
-
-
-
+#USER ACCOUNT CLASS
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)
@@ -40,15 +43,17 @@ class User(db.Model):
         self.password = password
 
 
-
-
+app.jinja_env.globals['momentjs'] = momentjs
+#BLOCKING ROUTES FOR USERS WHO ARE NOT SIGNED IN
+#JUSTIN CURRENTLY CREATES ACCOUNTS, NEW USERS ARE MADE BY HIM
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'register', 'static']
+    allowed_routes = ['login', 'static', 'register']
     if request.endpoint not in allowed_routes and 'email' not in session:
         return redirect('/login')
 
 
+#HOME PAGE ROUTE, SHOWS THE DASHBOARD FOR THE TASK MANAGEMENT APP
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
@@ -58,13 +63,15 @@ def index():
     completed_tasks = Task.query.filter_by(completed=True, owner=owner).all()
 
     return render_template('index.html',
+            timestamp = datetime.now().replace(minute = 0),
             owner=owner,
             tasks=tasks,
             completed_tasks=completed_tasks,
             quad_id=quad_id,
             title="Bare Necessities, Bitch.",)
 
-#THIS ROUTE BRINGS THE USER TO THE HOME PAGE TO CHOOSE HOW TO FILTER THEIR TASKS.
+
+#THIS ROUTE BRINGS THE USER TO THE TASKS PAGE, FILTERED BY WHICH QUADRANT THEY CLICK INTO
 @app.route('/bn', methods=['POST', 'GET'])
 def bn():
 
@@ -73,19 +80,19 @@ def bn():
     completed_tasks = Task.query.filter_by(completed=True, owner=owner).all()
     quad_id = request.args.get('quad_id')
 
-    #Captures the new task and adds it to the databse
+    #IF THE USER WANTS TO CREATE A NEW TASK
     if request.method == 'POST':
         task_name = request.form['task']
         new_task = Task(task_name, owner, quad_id)
         db.session.add(new_task)
         db.session.commit()
 
-    #When clicking on a quadrant, this will assign the quad_id variable with a value of 1-6
-
-
-    #quadrant2 filter
+    #CHECK WHICH QUADRANT THE USER CLICKED INTO
     if (quad_id):
 
+        #USER THE OWNER ACCOUNT SIGNED IN TO ONLY FILTER ONLY THEIR TASKS
+        #FILTER BY COMPLETED TRUE VS FALSE TO SHOW BOTH CATEGORIES.
+        #USE THE QUAD_ID VARIABLE TO FILTER THE TASKS
         quad_tasks = Task.query.filter_by(quad_id=quad_id, owner=owner, completed=False).all()
         completed_quad_tasks = Task.query.filter_by(quad_id=quad_id, owner=owner, completed=True).all()
 
@@ -95,31 +102,93 @@ def bn():
                         quad_id=quad_id,
                         completed_quad_tasks=completed_quad_tasks,)
 
-    return render_template('bn.html',
-          title="Get Er' Done",
-          tasks=tasks,
-          completed_tasks=completed_tasks)
+        #TODO create a 404 / error page to land on
+    return render_template('error_page.html',
+          title="Error")
 
 
+#APP LANDS ON THIS PAGE IF USER IS NOT CURRENTLY SIGNED IN
+#CHECKS TO MAKE SURE USER IS REGISTED IN THE DATABASE
+#CHECKS THAT THE PASSWORD MATCHES THE USERS PASSWORD SAVED
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        user = User.query.filter_by(email=email).first()
-        if user and user.password == password:
-            # to do - "remember that the user has logged in"
-            session['email'] = email
-            flash("Logged in")
-            # session['password'] = password
-            return redirect('/')
-        else:
-            flash('User password Incorrect, or user does not exist', 'error')
-
+        #Checks if there is a user email that matches the provided login
+        if not (User.query.filter_by(email=email).first()):
+            flash("username is incorrect or not registered.")
+            return render_template('login.html')
+        #if there is a user, get the user from the database
+        #verify the password provided matches the database password
+        if (User.query.filter_by(email=email).first()):
+            check_password = User.query.filter_by(email=email).first()
+            if password != check_password.password:
+                flash("password is incorrect.")
+                #KEEP THE USERNAME IF IT WAS CORRECT
+                return render_template('login.html', check_password=check_password.email)
+            #if password matches, log user in
+            if password == check_password.password:
+                session['email'] = email
+                flash("Logged in")
+                return redirect('/')
 
     return render_template('login.html')
 
+#ROUTE HANDLER IS ORDER TO CHANGE COMPLETED TASK ATTRIBUTE OF TRUE/FALSE
+@app.route('/complete-task', methods=['POST'])
+def complete_task():
 
+    task_id = int(request.form['task-id'])
+    task = Task.query.get(task_id)
+    task.completed = True
+    db.session.add(task)
+    db.session.commit()
+    url_id = task.quad_id
+
+    url = '/bn?quad_id=' + str(url_id)
+
+    return redirect(url)
+
+#ROUTE HANDLER IS ORDER TO CHANGE COMPLETED TASK ATTRIBUTE OF TRUE/FALSE
+@app.route('/un-complete-task', methods=['POST'])
+def un_complete_task():
+
+    task_id = int(request.form['task-id'])
+    task = Task.query.get(task_id)
+    task.completed = False
+    db.session.add(task)
+    db.session.commit()
+    url_id = task.quad_id
+
+    url = '/bn?quad_id=' + str(url_id)
+
+    return redirect(url)
+
+#REMOVE TASK FROM THE DATABASE
+@app.route('/delete-task', methods=['POST'])
+def delete_task():
+
+    task_id = int(request.form['task-id'])
+    task = Task.query.get(task_id)
+    task.completed = True
+    db.session.delete(task)
+    db.session.commit()
+    url_id = task.quad_id
+
+    url = '/bn?quad_id=' + str(url_id)
+
+    return redirect(url)
+
+#LOGS USER OF OF THE SESSION
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect('/')
+
+
+
+# NEED TO DELETE THIS WHEN WE LAUNCH AND HAVE A WAY TO MANUALLY ENTER USERS
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
@@ -142,26 +211,6 @@ def register():
             return "<h1>Duplicate User</h1>"
 
     return render_template('register.html')
-
-
-@app.route('/delete-task', methods=['POST'])
-def delete_task():
-
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
-    db.session.commit()
-    url_id = task.quad_id
-
-    url = '/bn?quad_id=' + str(url_id)
-
-    return redirect(url)
-
-@app.route('/logout')
-def logout():
-    del session['email']
-    return redirect('/')
 
 
 if __name__ == '__main__':
