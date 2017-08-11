801cf5557f6f74b12dec6b3e777c3601ace9353c
Adam Reed <adam.rdconcepts@gmail.com>
2017-08-10 22:47:21 -0700

    MVP Done

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from datetime import datetime
 from momentjs import momentjs
 
 
-
+#SET UP FLASK APP
 app = Flask(__name__)
 app.config['DEBUG'] = True
+#SET ROUTE FOR DATABASE
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done@localhost:8889/get-it-done'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-app.secret_key = "abc"
+app.secret_key = "thebarenecessities"
 
 #TASK USER CLASS, CURRENTLY FILTERS BY
     # USER ACCOUNT
@@ -46,6 +47,8 @@ class User(db.Model):
 
 
 app.jinja_env.globals['momentjs'] = momentjs
+
+
 #BLOCKING ROUTES FOR USERS WHO ARE NOT SIGNED IN
 #JUSTIN CURRENTLY CREATES ACCOUNTS, NEW USERS ARE MADE BY HIM
 @app.before_request
@@ -55,33 +58,33 @@ def require_login():
         return redirect('/login')
 
 
+#APP LANDS ON THIS PAGE IF USER IS NOT CURRENTLY SIGNED IN
+#CHECKS TO MAKE SURE USER IS REGISTED IN THE DATABASE
+#CHECKS THAT THE PASSWORD MATCHES THE USERS PASSWORD SAVED
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        #Checks if there is a user email that matches the provided login
+        if not (User.query.filter_by(email=email).first()):
+            flash("username is incorrect or not registered.")
+            return render_template('login.html')
+        #if there is a user, get the user from the database
+        #verify the password provided matches the database password
+        if (User.query.filter_by(email=email).first()):
+            check_password = User.query.filter_by(email=email).first()
+            if password != check_password.password:
+                flash("password is incorrect.")
+                #KEEP THE USERNAME IF IT WAS CORRECT
+                return render_template('login.html', check_password=check_password.email)
+            #if password matches, log user in
+            if password == check_password.password:
+                session['email'] = email
+                flash("Logged in")
+                return redirect('/')
 
-@app.route('/pdf_template', methods=['POST', 'GET'])
-def pdf_templates():
-
-    owner = User.query.filter_by(email=session['email']).first()
-    tasks = Task.query.filter_by(completed=False, owner=owner).all()
-
-    options = {
-        'page-size' : 'Letter',
-        'margin-top': '0in',
-        'margin-right': '0in',
-        'margin-bottom': '0in',
-        'margin-left': '0in',
-    }
-    css = 'static/styles.css'
-
-    rendered = render_template('pdf_template.html',
-                                owner = owner,
-                                tasks=tasks,
-                                )
-
-    pdf = pdfkit.from_string(rendered, False, options=options)
-    response = make_response(pdf)
-    response.headers['Content-Type'] = 'application/pdf'
-    response.headers['Content-Disposition'] = 'inline'
-
-    return response
+    return render_template('login.html')
 
 
 
@@ -139,34 +142,75 @@ def bn():
           title="Error")
 
 
-#APP LANDS ON THIS PAGE IF USER IS NOT CURRENTLY SIGNED IN
-#CHECKS TO MAKE SURE USER IS REGISTED IN THE DATABASE
-#CHECKS THAT THE PASSWORD MATCHES THE USERS PASSWORD SAVED
-@app.route('/login', methods=['POST', 'GET'])
-def login():
+#PRINT YOUR BN!
+@app.route('/pdf_template', methods=['POST', 'GET'])
+def pdf_templates():
+
+    owner = User.query.filter_by(email=session['email']).first()
+    tasks = Task.query.filter_by(completed=False, owner=owner).all()
+
+    options = {
+        'page-size' : 'Letter',
+        'margin-top': '0in',
+        'margin-right': '0in',
+        'margin-bottom': '0in',
+        'margin-left': '0in',
+    }
+    css = 'static/styles.css'
+
+    rendered = render_template('pdf_template.html',
+                                owner = owner,
+                                tasks=tasks,
+                                )
+
+    pdf = pdfkit.from_string(rendered, False, options=options)
+    response = make_response(pdf)
+    response.headers['Content-Type'] = 'application/pdf'
+    response.headers['Content-Disposition'] = 'inline'
+
+    return response
+
+
+# TODO Make this behind a password somehow?
+@app.route('/really-long-registration_name-throws-off-hackers', methods=['POST', 'GET'])
+def register():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        #Checks if there is a user email that matches the provided login
-        if not (User.query.filter_by(email=email).first()):
-            flash("username is incorrect or not registered.")
-            return render_template('login.html')
-        #if there is a user, get the user from the database
-        #verify the password provided matches the database password
-        if (User.query.filter_by(email=email).first()):
-            check_password = User.query.filter_by(email=email).first()
-            if password != check_password.password:
-                flash("password is incorrect.")
-                #KEEP THE USERNAME IF IT WAS CORRECT
-                return render_template('login.html', check_password=check_password.email)
-            #if password matches, log user in
-            if password == check_password.password:
-                session['email'] = email
-                flash("Logged in")
-                return redirect('/')
+        verify = request.form['verify']
 
-    return render_template('login.html')
+        # - Validate the users data
 
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            #todo - Remember the user
+            session['email'] = email
+            return redirect('/')
+        else:
+            # todo - user better response message
+            return "<h1>Duplicate User</h1>"
+
+    return render_template('register.html')
+
+
+#LOGS USER OF OF THE SESSION
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect('/')
+
+
+
+
+
+
+
+#######################
+## FUTURE FEATURES  ###
+#######################
 #ROUTE HANDLER IS ORDER TO CHANGE COMPLETED TASK ATTRIBUTE OF TRUE/FALSE
 @app.route('/complete-task', methods=['POST'])
 def complete_task():
@@ -212,37 +256,6 @@ def delete_task():
 
     return redirect(url)
 
-#LOGS USER OF OF THE SESSION
-@app.route('/logout')
-def logout():
-    del session['email']
-    return redirect('/')
-
-
-
-# NEED TO DELETE THIS WHEN WE LAUNCH AND HAVE A WAY TO MANUALLY ENTER USERS
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        verify = request.form['verify']
-
-        # - Validate the users data
-
-        existing_user = User.query.filter_by(email=email).first()
-        if not existing_user:
-            new_user = User(email, password)
-            db.session.add(new_user)
-            db.session.commit()
-            #todo - Remember the user
-            session['email'] = email
-            return redirect('/')
-        else:
-            # todo - user better response message
-            return "<h1>Duplicate User</h1>"
-
-    return render_template('register.html')
 
 
 if __name__ == '__main__':
